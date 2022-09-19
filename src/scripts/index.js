88c3d13293841e1a0ccead6b68a9d0d7968ef71a
Matheus Macedo <matheusg3.201168@gmail.com>
2022-09-19 19:19:21 -0300

    fisrt commit

---
+++ src/scripts/index.js
@@ -0,0 +1,91 @@
+import { getRepositories } from '/src/scripts/services/repositories.js'
+import { getUser } from '/src/scripts/services/user.js'
+
+import { user } from '/src/scripts/objects/user.js'
+import { screen } from '/src/scripts/objects/screen.js'
+
+document.getElementById("btn-search").addEventListener("click", () => {
+  const userName = document.getElementById("input-search").value;
+  if(renderNotFound(userName)) return
+  getUserData(userName);
+});
+
+document.getElementById("input-search").addEventListener("keyup", (e) => {
+  const userName = e.target.value;
+  const key = e.which || e.keyCode;
+  const enterKeyPressed = key === 13;
+
+  if (enterKeyPressed) {
+    if(renderNotFound(userName)) return
+    getUserData(userName);
+  }
+});
+
+function renderNotFound(userName){
+  if(userName.length === 0){
+    alert('Preencha o campo com o nome do usuário do GitHub')
+    return true
+  }
+}
+
+async function getUserData(userName) {
+
+  const userResponse = await getUser(userName);
+
+  if(userResponse.message === 'Not Found'){
+    screen.renderNotFound()
+    return
+  }
+
+  const repositoriesResponse = await getRepositories(userName);
+
+  user.setInfo(userResponse);
+  user.setRepositories(repositoriesResponse)
+
+  screen.renderUser(user)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getUserRepositories(userName) {
+//   getrepositories(userName).then((reposData) => {
+//     let repositoriesItens = "";
+//     reposData.forEach((repo) => {
+//       repositoriesItens += `<li>
+//                               <a href="${repo.html_url}" target="_blank">
+//                                 ${repo.name}
+//                               </a>  
+//                             </li>                      
+//                             `;
+//     });
+//     document.querySelector(".profile-data")
+//     .innerHTML +=   `   <div class="repositories section">
+//                             <h2>Repositorios</h2>
+//                             <ul>
+//                             ${repositoriesItens}      
+//                             </ul>
+//                         </div>
+//                     `;
+//   });
+// }
---
+++ src/scripts/objects/screen.js
@@ -0,0 +1,36 @@
+const screen = {
+  userProfile: document.querySelector(".profile-data"),
+  renderUser(user) {
+    this.userProfile.innerHTML = `<div class="info">
+                         <img src="${user.avatarUrl}">
+                         <div class="data">
+                             <h1>
+                             ${user.name ?? "Não possui nome cadastrado"}
+                             </h1>
+                             <p>
+                             ${user.bio ?? "Não possui bios cadastrada "}
+                             </p>
+                         </div>
+                    </div>`;
+
+    let repositoriesItens = "";
+
+    user.repositories.forEach( repo =>
+        (repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`)
+    );
+    console.log(repositoriesItens);
+
+    if (user.repositories.length > 0) {
+      this.userProfile.innerHTML += `<div class="repositories section">
+                                        <h2>Repositorios</h2>
+                                        <ul>${repositoriesItens}</ul>
+                                     </div>`;
+    }
+  },
+
+  renderNotFound(){
+    this.userProfile.innerHTML = `<h3>Usuário não encontrado</h3>`
+  }
+};
+
+export { screen };
